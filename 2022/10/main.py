db55eb7334b7e78fed9cca564dbc8820d4c15b91
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-12-10 16:50:44 +0100

    2022 Day 10 part 2

--- 2022/10/main.py
+++ 2022/10/main.py
@@ -5,9 +5,7 @@ def part_1():
     x_register = 1
     program = []
     with open("input.txt") as indata:
-        for line in indata:
-            program.append(line)
-
+        program.extend(iter(indata))
     cpu_busy = False
     addx = re.compile(r"addx (-?\d+)")
     noop = re.compile("noop")
@@ -22,20 +20,43 @@ def part_1():
             continue
         else:
             instruction = program.pop(0)
-            if m := addx.match(instruction):
-                add_value = int(m[1])
-                cpu_busy = True
-            else:
+            if not (m := addx.match(instruction)):
                 continue
-        print(f"End of cycle {clock_cycle}, value of register is {x_register}, cpu {'not' if not cpu_busy else ''} is busy")
+            add_value = int(m[1])
+            cpu_busy = True
+
     print(sum(signal_strengths))
 
 
 def part_2():
+    x_register = 1
+    program = []
     with open("input.txt") as indata:
-        for line in indata:
-            ...
+        program.extend(iter(indata))
+    cpu_busy = False
+    addx = re.compile(r"addx (-?\d+)")
+    noop = re.compile("noop")
+    add_value = 0
+    screen = [["."] * 40 for _ in range(6)]
+    for clock_cycle in range(1, 240):
+        current_pixel = (clock_cycle - 1) % 40
+        if current_pixel in (x_register - 1, x_register, x_register + 1):
+            current_row = clock_cycle // 40
+            screen[current_row][current_pixel] = "#"
+        if cpu_busy:
+            cpu_busy = False
+            x_register += add_value
+            continue
+        else:
+            instruction = program.pop(0)
+            if not (m := addx.match(instruction)):
+                continue
+            add_value = int(m[1])
+            cpu_busy = True
+
+    for row in screen:
+        print("".join(row))
 
 
 if __name__ == '__main__':
-    part_1()
+    part_2()
