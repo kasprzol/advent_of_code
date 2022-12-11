5d3c4ffd944f6d3a8bdea085a344467e920b973a
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-12-11 17:24:08 +0100

    2022 Day 11 part 2

--- 2022/11/main.py
+++ 2022/11/main.py
@@ -23,14 +23,13 @@ def parse_monkey(indata: TextIOBase) -> tuple[Monkey, bool]:
     monkey_id = re.compile(r"Monkey (?P<id>\d+):")
     items = re.compile(r"Starting items: (?P<items>[\d, ]+)")
     operation = re.compile(r"Operation: new = old (?P<op>\*|\+|-) (?P<arg>\d+)")
-    operation_same_arg = re.compile(r"Operation: new = old (?P<op>\*|\+|-) old")
+    operation_same_arg = re.compile(r"Operation: new = old (?P<op>\*) old")
     test = re.compile(r"Test: divisible by (?P<arg>\d+)")
     if_true = re.compile(r"If true: throw to monkey (?P<arg>\d+)")
     if_false = re.compile(r"If false: throw to monkey (?P<arg>\d+)")
     operations = {
         "+": operator.add,
         "*": operator.mul,
-        "-": operation.sub
     }
     for line in indata:
         line = line.strip()
@@ -87,10 +86,40 @@ def part_1():
 
 
 def part_2():
+    monkeys = []
+    end_of_file = False
     with open("input.txt") as indata:
-        for line in indata:
-            ...
+        while not end_of_file:
+            monkey, end_of_file = parse_monkey(indata)
+            monkeys.append(monkey)
+    print(monkeys)
+    num_of_inspections = {monkey.id_: 0 for monkey in monkeys}
+    divider = 1
+    for monkey in monkeys:
+        divider *= monkey.test_divisible_arg
+
+    num_of_rounds = 10_000
+    # num_of_rounds = 151
+    for round in range(num_of_rounds):
+        for monkey in monkeys:
+            while monkey.items:
+                num_of_inspections[monkey.id_] += 1
+                item = monkey.items.pop(0)
+                if monkey.operation_arg == Monkey.same_arg_placeholder:
+                    item = monkey.operation(item, item)
+                else:
+                    item = monkey.operation(item, monkey.operation_arg)
+                item = item % divider
+                target = monkey.if_true_target if item % monkey.test_divisible_arg == 0 else monkey.if_false_target
+                monkeys[target].items.append(item)
+        if (round + 1) % 150 == 0:
+            print(f"After {round=}")
+            for monkey in monkeys:
+                print(f"{monkey.id_}: {monkey.items}")
+    print(num_of_inspections)
+    top2 = (sorted(num_of_inspections.values(), reverse=True))[:2]
+    print(top2[0] * top2[1])
 
 
 if __name__ == '__main__':
-    part_1()
+    part_2()
