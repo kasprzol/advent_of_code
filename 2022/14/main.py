852c40635567bdd1fb4b45799698f37316058565
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-12-15 18:52:31 +0100

    2022 day 14 part 2.

--- 2022/14/main.py
+++ 2022/14/main.py
@@ -102,9 +102,65 @@ def load_cave() -> list[list[Point]]:
     return segments
 
 
+def simulate_sand_2(cave, cave_min_x, cave_max_x, cave_max_y):
+    print_cave(cave, cave_max_y)
+    is_current_sand_moving = False
+    reached_start = False
+    active_sand = None
+    units_of_sand = 0
+    floor = cave_max_y + 2
+    while not reached_start:
+        if not is_current_sand_moving:
+            units_of_sand += 1
+            active_sand = Point(SAND_START_X, SAND_START_Y)
+            is_current_sand_moving = True
+
+        while cave[active_sand.x][active_sand.y + 1] == EMPTY and active_sand.y + 1 < floor:
+            active_sand = Point(active_sand.x, active_sand.y + 1)
+            if active_sand.y >= floor:
+                is_current_sand_moving = False
+                break
+
+        # can move left-down?
+        if active_sand.y + 1 < floor and cave[active_sand.x - 1][active_sand.y + 1] == EMPTY:
+            active_sand = Point(active_sand.x - 1, active_sand.y + 1)
+        # can move right-down?
+        elif active_sand.y + 1 < floor and cave[active_sand.x + 1][active_sand.y + 1] == EMPTY:
+            active_sand = Point(active_sand.x + 1, active_sand.y + 1)
+        else:
+            # the sand had rested
+            cave[active_sand.x][active_sand.y] = SAND
+            is_current_sand_moving = False
+            if units_of_sand % 100 == 0:
+                print_cave(cave, floor, active_sand)
+            if active_sand.x == SAND_START_X and active_sand.y == SAND_START_Y:
+                print_cave(cave, floor, active_sand)
+                print(units_of_sand)
+                return
+
+
 def part_2():
-    ...
+    segments = load_cave()
+    cave = defaultdict(lambda: defaultdict(lambda: EMPTY))
+    cave_min_x, cave_max_x, cave_max_y = 99999, 0, 0
+    for segment in segments:
+        for points in pairwise(segment):
+            cave_min_x = min((cave_min_x, points[0].x, points[1].x))
+            cave_max_x = max((cave_max_x, points[0].x, points[1].x))
+            cave_max_y = max((cave_max_y, points[0].y, points[1].y))
+            if points[0].x == points[1].x:
+                min_y = min(points[0].y, points[1].y)
+                max_y = max(points[0].y, points[1].y)
+                for y in range(min_y, max_y + 1):
+                    cave[points[0].x][y] = ROCK
+            else:
+                min_x = min(points[0].x, points[1].x)
+                max_x = max(points[0].x, points[1].x)
+                for x in range(min_x, max_x + 1):
+                    cave[x][points[0].y] = ROCK
+    cave[500][0] = SAND_START
+    simulate_sand_2(cave, cave_min_x, cave_max_x, cave_max_y)
 
 
 if __name__ == "__main__":
-    part_1()
+    part_2()
