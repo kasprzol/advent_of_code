ff79ea699e23c46a20d99340127e26f20b545753
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-12-21 12:27:42 +0100

    2022 day 18 part 2.

--- utils.py
+++ utils.py
@@ -1,10 +1,36 @@
+from typing import NamedTuple
+
 from dataclasses import dataclass
 
 
-@dataclass
-class Point:
+class Point(NamedTuple):
     x: int
     y: int
+
+
+class Point3d(NamedTuple):
+    x: int
+    y: int
+    z: int
+
+
+@dataclass
+class MutablePoint:
+    x: int
+    y: int
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+@dataclass
+class MutalbePoint3d:
+    x: int
+    y: int
+    z: int
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
 
 
 @dataclass
