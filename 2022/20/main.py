b225a55db48ff745a47b70b71b4850c485cfdfba
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2023-01-28 00:25:10 +0100

    2022 day 20 part 2 WIP

--- 2022/20/main.py
+++ 2022/20/main.py
@@ -52,5 +52,56 @@ def part_1():
     print(coordinates)
 
 
+def part_2():
+    decryption_key = 811589153
+    number_of_rounds = 10
+    indata = load_input()
+    numbers_with_original_indexes = [(num * decryption_key, idx) for idx, num in enumerate(indata)]
+    orig_index_to_num = {idx: num for (num, idx) in numbers_with_original_indexes}
+    numbers = numbers_with_original_indexes.copy()
+    # print(f"Starting buffer: {numbers}")
+    len_numbers_1 = len(numbers) - 1
+    for round in range(number_of_rounds + 1):
+        for i in range(len(numbers_with_original_indexes)):
+            # find the new index of the number with original index i
+            current_index = numbers.index((orig_index_to_num[i], i))
+            number_and_idx = numbers[current_index]
+            shift = number_and_idx[0]
+            wrap_around_times = abs(shift) // len_numbers_1
+            shift += wrap_around_times * len_numbers_1 * (1 if shift < 0 else -1)
+            new_index = current_index + shift
+            # if the last element is going forward then need to move it one more
+            # step, as it's also the first item (right before the 1st item in a
+            # circular buffer).
+            if current_index == len_numbers_1 and shift > 0:
+                new_index += 1
+            if new_index < 0:
+                # -1 because the 1st item (index 0) is already the last item
+                # (i.e. it's after the last item because it's a circular buffer)
+                new_index += len_numbers_1
+            new_index %= len_numbers_1
+            if VERBOSE > 2:
+                print(f"Moving number {number_and_idx[0]} from index {current_index} to {new_index}")
+            del numbers[current_index]
+            numbers.insert(new_index, number_and_idx)
+            if VERBOSE > 1:
+                print(f"Current buffer: {numbers}")
+        if VERBOSE > 0:
+            print(f"Buffer after {round=}: {numbers}")
+
+    zero = [num for num in numbers if num[0] == 0]
+    assert len(zero) == 1
+    zero_idx = numbers.index(zero[0])
+    if VERBOSE > 0:
+        print(f"Found zero at index x")
+    coordinates_idx = (
+        (zero_idx + 1000) % len(numbers),
+        (zero_idx + 2000) % len(numbers),
+        (zero_idx + 3000) % len(numbers),
+    )
+    coordinates = numbers[coordinates_idx[0]][0] + numbers[coordinates_idx[1]][0] + numbers[coordinates_idx[2]][0]
+    print(coordinates)
+
+
 if __name__ == "__main__":
     part_1()
