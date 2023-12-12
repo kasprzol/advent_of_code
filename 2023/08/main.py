5cda7f3c94c1ee734b40143c52ee68d37780866a
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2023-12-12 21:41:10 +0100

    3034 day 08 part 2

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -3,8 +3,10 @@ import dataclasses
 from typing import NamedTuple
 from collections.abc import Iterable, Sequence
 import itertools
+import functools
 import tqdm
 
+from math import lcm
 
 
 def part1():
@@ -34,13 +36,44 @@ def part1():
 
 ################################################################################
 
-def part2():
-    value = 0
+def get_route_length(starting_point, nodes, instructions):
+    current_node = starting_point
+    for idx, instruction in enumerate(itertools.cycle(instructions), 1):
+        current_node = nodes[current_node][instruction]
+        if current_node[-1] == "Z":
+            return idx
 
+
+def part2():
+    nodes = {}
     for line in open("input.txt").readlines():
         line = line.strip()
+        if not line:
+            continue
+        if "=" not in line:
+            instructions = line
+        else:
+            node, children = line.split("=")
+            node = node.strip()
+            children = children.strip()
+            nodes[node] = children.removeprefix("(").removesuffix(")").split(",")
+            nodes[node] = [n.strip() for n in nodes[node]]
+
+    instructions = [0 if i == "L" else 1 for i in instructions]
+    # current_nodes = [n for n in nodes if n.endswith("A")]
+    # for idx, instruction in tqdm.tqdm(enumerate(itertools.cycle(instructions), 1)):
+    #     current_nodes = [nodes[n][instruction] for n in current_nodes]
+    #     if all(n.endswith("Z") for n in current_nodes):
+    #         break
+    starting_points = [n for n in nodes if n.endswith("A")]
+    print(f"Starting nodes: {starting_points}")
+    route_lengths = [get_route_length(node, nodes, instructions) for node in starting_points]
+    print(route_lengths)
+    value = functools.reduce(lcm, route_lengths)
+
+
 
     print(f"The value is {value}")
 
 if __name__ == '__main__':
-    part1()
+    part2()
