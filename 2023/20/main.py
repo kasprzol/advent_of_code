74b75829994027837e564792f6d3789161f187e3
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2023-12-30 17:56:27 +0100

    2023 day 20 part 2

--- 2023/20/main.py
+++ 2023/20/main.py
@@ -104,11 +104,55 @@ def part1():
 def part2():
     value = 0
 
-    for line in open("input.txt").readlines():
-        line = line.strip()
+    iterations = 0
 
-    print(f"The value is {value}")
+    modules = read_input()
+
+    sent_pulses = {"high": 0, "low": 0}
+
+    while True:
+        events_queue = collections.deque([("broadcaster", None, "low")])
+        iterations += 1
+        if iterations % 10_000 == 0:
+            print(".", end="")
+        while events_queue:
+            destination, source, pulse = events_queue.popleft()
+            sent_pulses[pulse] += 1
+
+            mod = modules[destination]
+            source_state = modules[source].state if source else None
+
+            if destination == "rx" and pulse == "low":
+                print(f"Delivered low pulse to [red]rx[/red] module on {iterations=}")
+                return
+            if VERBOSE >= 1:
+                print(f"{source} ({source_state}) --{pulse}-> {destination} ({mod.state})")
+            if mod.type == "b":  # broadcaster
+                events_queue.extend([(o, destination, pulse) for o in mod.outputs])
+            elif mod.type == "%":
+                if pulse == "high":
+                    continue
+                # else:
+                p = "high" if mod.state == "off" else "low"
+                mod.state = "on" if mod.state == "off" else "off"
+                events_queue.extend([(o, destination, p) for o in mod.outputs])
+            elif mod.type == "&":
+                mod.state[source] = pulse
+                if all(v == "high" for v in mod.state.values()):
+                    p = "low"
+                else:
+                    p = "high"
+                events_queue.extend([(o, destination, p) for o in mod.outputs])
+                # those are inputs to "dg", which is the only input to "rx". If all of those
+                # send a high pulse to "dg" it will trigger and send a low pulse to "rx".
+                # On iterations=3767 a high pulse was sent from xt.
+                # On iterations=3823 a high pulse was sent from lk.
+                # On iterations=3929 a high pulse was sent from sp.
+                # On iterations=4051 a high pulse was sent from zv.
+                # 3767*3823*2929*4051 == 229215609826339
+                if destination in ("lk", "zv", "sp", "xt") and p == "high":
+                    print(f"On {iterations=} a high pulse was sent from {destination}.")
 
 
 if __name__ == "__main__":
-    part1()
+    part2()
