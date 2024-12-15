1b532a95c699e428500d9118a0b4c3e006438a1b
Maciej "Kasprzol" Kasprzyk <git_blame@kasprzyk.tech>
2024-12-15 20:27:50 +0100

    2024 day 15 part 2

--- 2024/15/aoc15.py
+++ 2024/15/aoc15.py
@@ -24,6 +24,7 @@ WALL = "#"
 BOX = "O"
 EMPTY = "."
 
+
 class Point(NamedTuple):
     x: int
     y: int
@@ -61,6 +62,9 @@ class MutablePoint:
             return NotImplemented
         return not self == other
 
+    def copy(self):
+        return MutablePoint(self.x, self.y)
+
 
 class Direction(enum.StrEnum):
     UP = "^"
@@ -68,19 +72,16 @@ class Direction(enum.StrEnum):
     LEFT = "<"
     RIGHT = ">"
 
+
 DIRECTION_TO_VECTOR = {
     Direction.UP: Point(0, -1),
     Direction.DOWN: Point(0, 1),
     Direction.LEFT: Point(-1, 0),
     Direction.RIGHT: Point(1, 0),
-        }
+}
 
 
-def print_map(warehouse, robots: list[MutablePoint]):
-    robot_positions = defaultdict(int)
-    for r in robots:
-        robot_positions[Point(r.x, r.y)] += 1
-
+def print_map(warehouse, robot: MutablePoint):
     print("    ", end="")
     height = len(warehouse)
     width = len(warehouse[0])
@@ -90,15 +91,15 @@ def print_map(warehouse, robots: list[MutablePoint]):
     for y in range(height):
         print(f"{y:2}: ", end="")
         for x in range(width):
-            if (count := robot_positions[Point(x, y)]) > 0:
+            if robot == Point(x, y):
                 # print("\N{robot face}", end="")
                 print("[red]@[/red]", end="")
             else:
                 match warehouse[y][x]:
                     case "#":
                         print(f"[yellow]{WALL}[/yellow]", end="")
-                    case "O":
-                        print(f"[blue]{BOX}[/blue]", end="")
+                    case w if w in (BOX, "[", "]"):
+                        print(f"[blue]{w}[/blue]", end="")
                     case _:
                         print(warehouse[y][x], end="")
         print("")
@@ -108,7 +109,7 @@ def print_map(warehouse, robots: list[MutablePoint]):
 def load_input(indata: TextIOWrapper):
     map = []
     moves = []
-    robots = []
+    robot = None
     reading_map = True
     for row_idx, line in enumerate(indata):
         line = line.strip()
@@ -119,13 +120,12 @@ def load_input(indata: TextIOWrapper):
         if reading_map:
             if "@" in line:
                 col = line.index("@")
-                assert line.count("@") == 1
-                robots.append(MutablePoint(row_idx, col))
+                robot = MutablePoint(row_idx, col)
                 line = line.replace("@", ".")
             map.append(list(line))
         else:
             moves.extend(list(line))
-    return map, robots, moves
+    return map, robot, moves
 
 
 def move_robot(robot: MutablePoint, warehouse, direction: Direction) -> MutablePoint:
@@ -139,7 +139,9 @@ def move_robot(robot: MutablePoint, warehouse, direction: Direction) -> MutableP
     if warehouse[next_point.y][next_point.x] == EMPTY:
         return next_point
     # the robot is against a box - try to move it (and any other boxes behind it)
-    boxes_to_move = [next_point]  # lets keep them all here, but in the end I just need to remove the last one and add one in the front
+    boxes_to_move = [
+        next_point
+    ]  # lets keep them all here, but in the end I just need to remove the last one and add one in the front
     point_to_check = MutablePoint(next_point.x, next_point.y)
     while True:
         point_to_check += DIRECTION_TO_VECTOR[direction]
@@ -152,19 +154,18 @@ def move_robot(robot: MutablePoint, warehouse, direction: Direction) -> MutableP
             warehouse[boxes_to_move[0].y][boxes_to_move[0].x] = EMPTY
             return next_point
         boxes_to_move.append(point_to_check)
-        
+
 
 def part1(input_file: TextIOWrapper):
-    warehouse, robots, robot_moves = load_input(input_file)
-    robot_pos = robots[0]
+    warehouse, robot_pos, robot_moves = load_input(input_file)
     if VERBOSE:
-        print_map(warehouse, [robot_pos])
-    for move in tqdm(robot_moves):
+        print_map(warehouse, robot_pos)
+    for move in (robot_moves):
         if VERBOSE:
             print(f"Move: [blue]{move}[/blue]")
         robot_pos = move_robot(robot_pos, warehouse, move)
         if VERBOSE:
-            print_map(warehouse, [robot_pos])
+            print_map(warehouse, robot_pos)
     gps = 0
     for ridx, row in enumerate(warehouse):
         for cidx, c in enumerate(row):
@@ -173,10 +174,132 @@ def part1(input_file: TextIOWrapper):
     print(f"Part 1: {gps}")
 
 
+def load_input2(indata: TextIOWrapper):
+    map = []
+    moves = []
+    robot = None
+    reading_map = True
+    for row_idx, line in enumerate(indata):
+        line = line.strip()
+        map_row = []
+        if not line:
+            continue
+        if "#" not in line:
+            reading_map = False
+        if reading_map:
+            for point in line:
+                match point:
+                    case "#":
+                        map_row.extend(["#", "#"])
+                    case "O":
+                        map_row.extend(["[", "]"])
+                    case ".":
+                        map_row.extend([".", "."])
+                    case "@":
+                        map_row.extend([".", "."])
+                        robot = MutablePoint(len(map_row) - 2, row_idx)
+            map.append(map_row)
+
+        else:
+            moves.extend(list(line))
+    return map, robot, moves
+
+
+def can_move_box_vertical(box_left, warehouse, direction: Direction) -> bool | list[MutablePoint]:
+    # if true, return a list of boxes that have to be moved
+    ret = []
+    for source in (box_left, box_left + DIRECTION_TO_VECTOR[Direction.RIGHT]):
+        dest = source + DIRECTION_TO_VECTOR[direction]
+        destination_value = warehouse[dest.y][dest.x]
+        if destination_value == WALL:
+            return False
+        if destination_value == EMPTY:
+            ret.append(source.copy())
+        elif destination_value == "[":
+            if r := can_move_box_vertical(dest, warehouse, direction):
+                for i in r:
+                    if i not in ret:
+                        ret.append(i)
+                ret.append(source.copy())
+            else:
+                return r
+        else:
+            d = dest + DIRECTION_TO_VECTOR[Direction.LEFT]
+            if r := can_move_box_vertical(d, warehouse, direction):
+                for i in r:
+                    if i not in ret:
+                        ret.append(i)
+                ret.append(source.copy())
+            else:
+                return r
+    return ret if ret else False
+
+
+def move_robot2(robot: MutablePoint, warehouse, direction: Direction) -> MutablePoint:
+    """
+    Try to move robot in the given direction. If there are boxes on the way then move them if possible by updating the
+    warehouse. Return new (or old) robot position.
+    """
+    next_point = robot + DIRECTION_TO_VECTOR[direction]
+    if warehouse[next_point.y][next_point.x] == WALL:
+        return robot
+    if warehouse[next_point.y][next_point.x] == EMPTY:
+        return next_point
+    # the robot is against a box - try to move it (and any other boxes behind it)
+    boxes_to_move = [
+        next_point
+    ]  # lets keep them all here, but in the end I just need to remove the last one and add one in the front
+    point_to_check = MutablePoint(next_point.x, next_point.y)
+    if direction in (Direction.LEFT, Direction.RIGHT):
+        # trival case
+        while True:
+            point_to_check += DIRECTION_TO_VECTOR[direction]
+            if warehouse[point_to_check.y][point_to_check.x] == WALL:
+                return robot
+            if warehouse[point_to_check.y][point_to_check.x] == EMPTY:
+                # the robot can move the boxes in that direction
+                # TODO: now have to move all the boxes, as those consist of 2 parts.
+                warehouse[point_to_check.y][point_to_check.x] = warehouse[boxes_to_move[-1].y][boxes_to_move[-1].x]
+                for box1, box2 in itertools.pairwise(reversed(boxes_to_move)):
+                    warehouse[box1.y][box1.x] = warehouse[box2.y][box2.x]
+                warehouse[boxes_to_move[0].y][boxes_to_move[0].x] = EMPTY
+                return next_point
+            boxes_to_move.append(point_to_check.copy())
+    else:
+        # move boxes up or down
+        dest = (
+            next_point
+            if warehouse[next_point.y][next_point.x] == "["
+            else next_point + DIRECTION_TO_VECTOR[Direction.LEFT]
+        )
+        boxes_to_move = can_move_box_vertical(dest, warehouse, direction)
+        if boxes_to_move is False:
+            return robot
+        else:
+            for box in boxes_to_move:
+                dest = box + DIRECTION_TO_VECTOR[direction]
+                warehouse[dest.y][dest.x] = warehouse[box.y][box.x]
+                warehouse[box.y][box.x] = EMPTY
+            return next_point
 
 
 def part2(input_file: TextIOWrapper):
-    print(f"Part 2: 0")
+    warehouse, robot, robot_moves = load_input2(input_file)
+    if VERBOSE:
+        print_map(warehouse, robot)
+    for midx, move in enumerate(robot_moves):
+        if VERBOSE:
+            print(f"Move {midx}: [blue]{move}[/blue]")
+        robot = move_robot2(robot, warehouse, move)
+        assert warehouse[robot.y][robot.x] == EMPTY
+        if VERBOSE:
+            print_map(warehouse, robot)
+    gps = 0
+    for ridx, row in enumerate(warehouse):
+        for cidx, c in enumerate(row):
+            if c == "[":
+                gps += 100 * ridx + cidx
+    print(f"Part 2: {gps}")
 
 
 def main():
